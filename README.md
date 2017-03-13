62ff457d565360be44b715bb83d1eed150cec558
Alessandro <aseduto@hotmail.com>
2017-03-13 18:10:16 +0100

    First Working basic version

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # chunk-upload
-Browser to node js chunked uploader
+Browser to node.js chunked uploader
+
+This module has a client side and a server side.
+
+The client side can be used in a browser.
+
+In order to see a working sample:
+
+```javascript
+npm install
+npm run build
+npm start
+```
+
+Open your browser in http://localhost:3000/index.html
---
+++ src/test/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import uploader from "../server.js";
+
+var app = express();
+
+var port = 3000;
+
+app.use(express.static("sample"));
+app.use("/js", express.static("bin"));
+
+app.use("/upload", uploader());
+
+/*
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+*/
+
+
+
+app.put("/upload", (req, res) => {
+    
+    console.log("-------------****");
+       //console.log(JSON.stringify(req.headers));
+    console.log(req.uploader);
+    console.log("-------------**--");
+
+       
+
+    res.send("OK");
+
+    
+});
+
+app.listen(port, function () {
+    console.log("app listening on port " + port + "!");
+});
+
+
+
