2f56b96a77abb45666d89b418a63a0cfc69573d7
aseduto <aseduto@hotmail.com>
2019-05-11 16:49:09 +0200

    crc full test

--- test/unit/crc32.spec.js
+++ test/unit/crc32.spec.js
@@ -4,6 +4,33 @@ const CRC32    = require('../../src/core/crc').CRC32;
 
 describe('CRC32', () => {
     
+    it('should handle small ', () => {
+
+        let small_string = '0123456';
+
+        const buffer = new Buffer(small_string);
+
+        const crc32 = new CRC32();
+        crc32.update(buffer);
+        /*const crc =*/ crc32.finalize();
+    });    
+
+
+    it('should handle more than 10240', () => {
+
+        let long_string = '';
+
+        for(let i = 0; i < 10245; i++)
+            long_string += i.toString();
+
+        const crc32 = new CRC32();
+        crc32.update(long_string);
+        /*const crc =*/ crc32.finalize();
+        
+
+
+    });
+
     it('should compute same as crc32 command', () => {
        
         const data = {
